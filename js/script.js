e028476c0858673bde9b89baeb7fc3c0739bdfbb
Julio <thecesar165@gmail.com>
2018-10-31 21:53:25 -0300

    Segudna aula de js

---
+++ js/script.js
@@ -0,0 +1,68 @@
+// let texto = 'texto';
+// let numero = 1;
+// let booleano = true;
+
+// let nome = prompt('Qual o seu nome?');
+// let valor = prompt('Quanto valor do seu empréstimo?');
+
+
+// alert(`Olá ${nome}!`);
+// alert(`Você gostaria de emprestar R$${valor}, certo?`);
+
+// let pretendePagar = confirm('Você pretende pagar o seu emrpéstimo?');
+
+// alert('OK! Obrigador por nos visitar!');
+
+let lista = [1, 2, 3, 4, 4, 6, "texto1", "texto2", false];
+// lista.length - Mostra o tamanho da lista
+// lista.push(Mais algum valor) -  Adiciona valores à lista
+// Lista push(valor, valor, valor) - Adiciona mais valores na lista simultaneamente
+// lista.splice - Um comando de remover coisas da minha lista
+// lista.splice (5, 3, 1, 10) - Splice vai pegar o índice 5 e vai apagar (com o número 3) os índices 5,6 e 7 (Depois ele adiciona mais o 1 e o 10)
+// lista.pop() - Serve para apagar o último item da lista
+// lista.splice(0, 0, 1) Acesso o índice 0, apago nada (0) e depois adiciono 1 no lugar do 0
+// As listas são declaradas com colcolhetes
+
+
+// objeto = {""};
+// Os objetos são declarados com chaves
+// Para adicionar algo ao objeto depois eu uso colchetes 
+
+let objeto = {
+    nome: 'Felipe',
+    idade: 23,
+    programador: true
+};
+
+// Se eu quiser utiliziar algo do objeto, eu devo utiliziar o ponto:
+// objeto.nome
+// Tudo que no Javascript que não for Texto, número ou booleano - vira um objeto
+
+let joazinho = {
+    nome: 'João',
+    idade: 13,
+    namorada: undefined,
+    gatos:[
+        {
+            nome: 'Claudio',
+            raca: 'SRD'
+        },
+        {
+            nome: 'Gustavo',
+            raca: 'Tomba-Latas'
+        }
+    ]
+};
+
+let mariazinha = {
+        nome: 'Maria',
+        idade: 17,
+        namorado: undefined,
+        cachorros: [
+            {
+                name: 'Bob',
+                raca: 'labrador'
+            }
+        ]
+};
+
